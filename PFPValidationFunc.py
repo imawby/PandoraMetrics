5ec2d0b8e5e5a35ea25c5878bc9f021369bedc53
Isobel Mawby <isobel.mawby@gmail.com>
2026-01-13 11:50:19 +0000

    Cleaning up PFP

--- PFPValidationFunc.py
+++ PFPValidationFunc.py
@@ -8,21 +8,6 @@ plot_dir = '/Users/isobel/Desktop/DUNE/2026/PandoraValidation/PFPValPlots/'
 ##############################################################################################
 ##############################################################################################
 
-class PlotVar :
-    def __init__(self, tree_name, x_label, range, n_bins):
-        self.tree_name = tree_name
-        self.x_label = x_label
-        self.range = range
-        self.n_bins = n_bins
-
-n_mc_hits_2d_var = PlotVar('MCP_NMCHits2D', 'NMCHits2D', [0,3000], 30)
-theta_xz_var = PlotVar('MCP_TrueThetaXZ', 'ThetaXZ', [-3.5, 3.5], 25)
-theta_yz_var = PlotVar('MCP_TrueThetaYZ', 'ThetaYZ', [-1.6, 1.6], 12)
-pfo_energy_var = PlotVar('MCP_TrueEnergy', 'True Particle Energy', [0,3], 50)
-        
-##############################################################################################
-##############################################################################################
-
 def TrackShowerClassification(int_masks, tier_masks, pdg_masks, pfp_branches) :
     
     for tier in Definitions.tiers :
@@ -77,52 +62,6 @@ def TrackShowerClassification(int_masks, tier_masks, pdg_masks, pfp_branches) :
         plt.show()
     
         file_name = f'TrackShowerClassification_{Definitions.tier_strings[tier]}'
-        fig.savefig(f'{plot_dir}{file_name}.pdf', bbox_inches='tight')
-
-
-#####################################################################################################################################################
-#####################################################################################################################################################
-
-def CompletenessPurity(int_masks, tier_masks, pdg_masks, pfp_branches, metric_string) :
-
-    mc_metric = pfp_branches[metric_string]
-    
-    for tier in Definitions.tiers :
-
-        tier_mask = tier_masks[tier]
-    
-        # Save plots for each tier in a different file
-        fig, ax = plt.subplots(ncols=len(Definitions.pdgs), nrows=len(Definitions.ints), figsize=(14, 10))
-        
-        for int_type in Definitions.ints :
-            
-            int_mask = int_masks[int_type]
-    
-            for iPDG in range(len(Definitions.pdgs)) :
-    
-                pdg = Definitions.pdgs[iPDG]
-                pdg_string = Definitions.pdg_strings[pdg]
-                colour = Definitions.pdg_color[pdg]
-                    
-                target_mask = int_mask & pdg_masks[pdg] & tier_mask
-                reconstructable_entries = ak.to_numpy(ak.flatten(mc_metric[target_mask]))
-                n_reconstructable_entries = len(reconstructable_entries)
-                weights = np.ones(n_reconstructable_entries) * (1.0 / n_reconstructable_entries)
-                ax[int_type][iPDG].hist(reconstructable_entries, bins=20, range=[0.0,1.0], weights=weights, histtype='step', color=colour, linewidth=1, label=(f' {pdg_string} '))
-                ax[int_type][iPDG].legend()
-                ax[int_type][iPDG].set_ylim(0.0, 1.0)
-                is_x_label_index = int_type == (len(Definitions.ints) - 1)
-                ax[int_type][iPDG].set_xlabel(metric_string if is_x_label_index else '')
-                ax[int_type][iPDG].tick_params(labelbottom=is_x_label_index, bottom=is_x_label_index)
-                is_y_label_index = (iPDG == 0)
-                ax[int_type][iPDG].set_title(f'       {Definitions.int_strings[int_type]}: {Definitions.tier_strings[tier]} {metric_string}' if is_y_label_index else '')
-                ax[int_type][iPDG].set_ylabel('Frac. of MCParticles' if is_y_label_index else '')
-                ax[int_type][iPDG].tick_params(labelleft=is_y_label_index, left=is_y_label_index)
-                ax[int_type][iPDG].grid(True)
-                fig.tight_layout(pad=0)
-                fig.subplots_adjust(left=0.08, bottom=0.08) 
-                
-        file_name = f'{metric_string}_{Definitions.tier_strings[tier]}'
         fig.savefig(f'{plot_dir}{file_name}.pdf', bbox_inches='tight')
 
 #####################################################################################################################################################
@@ -186,4 +125,102 @@ def RecoEfficiency(int_masks, tier_masks, pdg_masks, pfp_branches, plot_var) :
                 fig.subplots_adjust(left=0.08, bottom=0.08)
 
         file_name = f'Efficiency_{plot_var.tree_name}_{Definitions.tier_strings[tier]}'
+        fig.savefig(f'{plot_dir}{file_name}.pdf', bbox_inches='tight')
+
+#####################################################################################################################################################
+#####################################################################################################################################################
+
+def PlotVariable(int_masks, tier_masks, pdg_masks, pfp_branches, plot_var, only_matched) :
+
+    mc_metric = pfp_branches[plot_var.tree_name]
+
+    mc_has_match = ak.ones_like(mc_metric)
+    if (only_matched) :
+        mc_has_match = pfp_branches['MCP_HasMatch']
+    
+    for tier in Definitions.tiers :
+
+        tier_mask = tier_masks[tier]
+    
+        # Save plots for each tier in a different file
+        fig, ax = plt.subplots(ncols=len(Definitions.pdgs), nrows=len(Definitions.ints), figsize=(14, 10))
+
+        for int_type in Definitions.ints :
+        
+            int_mask = int_masks[int_type]
+    
+            for iPDG in range(len(Definitions.pdgs)) :
+    
+                pdg = Definitions.pdgs[iPDG]
+                pdg_string = Definitions.pdg_strings[pdg]
+                colour = Definitions.pdg_color[pdg]
+
+                target_mask = int_mask & pdg_masks[pdg] & tier_mask
+                reco_mask = target_mask & (mc_has_match == 1)
+                reco_entries = ak.to_numpy(ak.flatten(mc_metric[reco_mask]))
+                n_reco_entries = len(reco_entries)
+                weights = np.ones(n_reco_entries) * (1.0 / n_reco_entries)
+                
+                ax[int_type][iPDG].hist(reco_entries, bins=plot_var.n_bins, range=plot_var.range, weights=weights, histtype='step', color=colour, linewidth=1, label=(f' {pdg_string} '))
+                ax[int_type][iPDG].legend()
+                ax[int_type][iPDG].set_ylim(0.0, 1.0)
+                is_x_label_index = int_type == (len(Definitions.ints) - 1)
+                ax[int_type][iPDG].set_xlabel(plot_var.x_label if is_x_label_index else '')
+                ax[int_type][iPDG].tick_params(labelbottom=is_x_label_index, bottom=is_x_label_index)
+                is_y_label_index = (iPDG == 0)
+                ax[int_type][iPDG].set_title(f'       {Definitions.int_strings[int_type]}: {Definitions.tier_strings[tier]} {plot_var.tree_name}' if is_y_label_index else '')
+                ax[int_type][iPDG].set_ylabel(plot_var.y_label if is_y_label_index else '')
+                ax[int_type][iPDG].tick_params(labelleft=is_y_label_index, left=is_y_label_index)
+                ax[int_type][iPDG].grid(True)
+                fig.tight_layout(pad=0)
+                fig.subplots_adjust(left=0.08, bottom=0.08)
+
+        file_name = f'Efficiency_{plot_var.tree_name}_{Definitions.tier_strings[tier]}'
+        fig.savefig(f'{plot_dir}{file_name}.pdf', bbox_inches='tight')
+
+#####################################################################################################################################################
+#####################################################################################################################################################
+
+def PlotDiffVariable(int_masks, tier_masks, pdg_masks, pfp_branches, plot_diff_var) :
+
+    mc_metric = pfp_branches[plot_diff_var.true_tree_name] - pfp_branches[plot_diff_var.reco_tree_name]
+    mc_has_match = pfp_branches['MCP_HasMatch']
+    
+    for tier in Definitions.tiers :
+
+        tier_mask = tier_masks[tier]
+    
+        # Save plots for each tier in a different file
+        fig, ax = plt.subplots(ncols=len(Definitions.pdgs), nrows=len(Definitions.ints), figsize=(14, 10))
+
+        for int_type in Definitions.ints :
+        
+            int_mask = int_masks[int_type]
+    
+            for iPDG in range(len(Definitions.pdgs)) :
+    
+                pdg = Definitions.pdgs[iPDG]
+                pdg_string = Definitions.pdg_strings[pdg]
+                colour = Definitions.pdg_color[pdg]
+
+                target_mask = int_mask & pdg_masks[pdg] & tier_mask
+                reco_mask = target_mask & (mc_has_match == 1)
+                reco_entries = ak.to_numpy(ak.flatten(mc_metric[reco_mask]))
+                n_reco_entries = len(reco_entries)
+                weights = np.ones(n_reco_entries) * (1.0 / n_reco_entries)
+                ax[int_type][iPDG].hist(reco_entries, bins=plot_diff_var.n_bins, range=plot_diff_var.range, weights=weights, histtype='step', color=colour, linewidth=1, label=(f' {pdg_string} '))
+                ax[int_type][iPDG].legend()
+                ax[int_type][iPDG].set_ylim(0.0, 1.0)
+                is_x_label_index = int_type == (len(Definitions.ints) - 1)
+                ax[int_type][iPDG].set_xlabel(plot_diff_var.x_label if is_x_label_index else '')
+                ax[int_type][iPDG].tick_params(labelbottom=is_x_label_index, bottom=is_x_label_index)
+                is_y_label_index = (iPDG == 0)
+                ax[int_type][iPDG].set_title(f'       {Definitions.int_strings[int_type]}: {Definitions.tier_strings[tier]} {plot_diff_var.true_tree_name}-{plot_diff_var.reco_tree_name}' if is_y_label_index else '')
+                ax[int_type][iPDG].set_ylabel(plot_diff_var.y_label if is_y_label_index else '')
+                ax[int_type][iPDG].tick_params(labelleft=is_y_label_index, left=is_y_label_index)
+                ax[int_type][iPDG].grid(True)
+                fig.tight_layout(pad=0)
+                fig.subplots_adjust(left=0.08, bottom=0.08)
+
+        file_name = f'{plot_diff_var.true_tree_name}-{plot_diff_var.reco_tree_name}_{Definitions.tier_strings[tier]}'
         fig.savefig(f'{plot_dir}{file_name}.pdf', bbox_inches='tight')
